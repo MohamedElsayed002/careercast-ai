0221c544b5ce418ff0b096264afef81d7d31771a
MohamedElsayed002 <mohamed.madoo2016@gmail.com>
2025-11-03 22:07:31 +0200

    better-auth small modify

--- utils/auth.ts
+++ utils/auth.ts
@@ -11,6 +11,7 @@ export const polarClient = new Polar({
     server: 'sandbox'
 })
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
     database: prismaAdapter(prisma,{
         provider: 'postgresql'
     }),
